641dd671877698304534e95a7f53697c6a48c109
NickDgebuadze <nikadgeb@gmail.com>
2025-03-16 23:23:00 +0400

    changed contact setting

--- src/pages/index.js
+++ src/pages/index.js
@@ -151,12 +151,32 @@ export default function Home({ data = {} }) {
               </div>
               <div className={styles.sub_contact_desc}>
                 <div className={styles.contact_info_item}>
-                  <span className={styles.contact_label}>ტელეფონი:</span>
-                  <span className={styles.contact_value}>599 47 47 58</span>
+                  <span className={styles.contact_label}>
+                    საკონტაკტო პირი: დეკანოზი გიორგი კალანდია:
+                  </span>
+                  <div className={styles.phone_number}>
+                    <svg
+                      className={styles.phone_icon}
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                    </svg>
+                    <span className={styles.contact_value}>555-725-709</span>
+                  </div>
                 </div>
                 <div className={styles.contact_info_item}>
                   <span className={styles.contact_label}>მისამართი:</span>
-                  <span className={styles.contact_value}>უბანი ნომერი #3</span>
+                  <div className={styles.address_value}>
+                    თბილისი, ავლაბარი, სამრეკლოს ქუჩა, ყოვლადწმინდა სამების
+                    ლავრის ივერიის ღვთისმშობლის ტაძარი.
+                  </div>
                 </div>
                 <div className={styles.sub_desc}>
                   <a
